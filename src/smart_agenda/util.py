d2ff56f9d2a017f6bb28bbf1ae954579d90fa146
Jannis Mainczyk <jannis.mainczyk@iis.fraunhofer.de>
2022-08-18 00:32:29 +0200

    change: always append hash to agenda filename

--- src/smart_agenda/util.py
+++ src/smart_agenda/util.py
@@ -18,10 +18,9 @@ def get_filepath(agenda: Agenda, parent: Path = None) -> Path:
 
     # add hash to prevent overwriting different agenda with same title
     # same agendas (based on title & items) will produce same hash to avoid saving duplicates
-    if filepath.exists():
-        hash_content = ";".join((agenda.title, *(";".join((item.name, str(item.duration))) for item in agenda.items)))
-        agenda_hash = hashlib.md5(hash_content.encode()).hexdigest()
-        filepath = filepath.with_name(f"{filename}_{agenda_hash[:8]}")
+    hash_content = ";".join((agenda.title, *(";".join((item.name, str(item.duration))) for item in agenda.items)))
+    agenda_hash = hashlib.md5(hash_content.encode()).hexdigest()
+    filepath = filepath.with_name(f"{filename}_{agenda_hash[:8]}")
 
     logging.debug(filepath)
     return filepath
--- tests/test_util.py
+++ tests/test_util.py
@@ -16,6 +16,14 @@ def test_get_filepath_when_existing(tmp_path):
     fp.touch()
     assert fp.exists()
 
-    fp = util.get_filepath(agenda, parent=tmp_path)
-    assert isinstance(fp, Path)
-    assert not fp.exists(), "a unique filename should be provided"
+    agenda2 = Agenda("test", items=["item 1 2:00", "item 2 1:00"])
+    fp = util.get_filepath(agenda2, parent=tmp_path)
+    assert fp.exists(), "same agenda title and items should yield same filepath"
+
+    agenda3 = Agenda("different", items=["item 1 2:00", "item 2 1:00"])
+    fp = util.get_filepath(agenda3, parent=tmp_path)
+    assert not fp.exists(), "different agenda title should yield different filepath"
+
+    agenda3 = Agenda("test", items=["item 3 12:00", "item 4 11:00"])
+    fp = util.get_filepath(agenda3, parent=tmp_path)
+    assert not fp.exists(), "different agenda items should yield different filepath"
